e05bfeb88510e5c0490c898fa00e3857b75e12b8
sirdan <stanford303@gmail.com>
2022-08-03 14:57:15 -0700

    made module author

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,3 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP
+DROP table if exists authors;
+
+CREATE table authors (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    dob VARCHAR NOT NULL,
+    pob VARCHAR NOT NULL
+);
+
+INSERT INTO authors (name, dob, pob) VALUES 
+('tom lawyer', '8-jun-1772', 'west england')
+('billy bob', '12-july-1987', 'seattle  washington')
+('mike watts', '4-mar-1967', 'new york')
+('lip galmore', '5-dec-1994', 'west texas')
