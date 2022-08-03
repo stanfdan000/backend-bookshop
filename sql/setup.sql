4c0c3005373d070bcecb5a23d4b0784cacbb76a0
sirdan <stanford303@gmail.com>
2022-08-03 15:35:30 -0700

    passed auth test

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,3 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
 DROP table if exists authors;
 
 CREATE table authors (
@@ -10,7 +8,7 @@ CREATE table authors (
 );
 
 INSERT INTO authors (name, dob, pob) VALUES 
-('tom lawyer', '8-jun-1772', 'west england')
-('billy bob', '12-july-1987', 'seattle  washington')
-('mike watts', '4-mar-1967', 'new york')
-('lip galmore', '5-dec-1994', 'west texas')
+('tom lawyer', '8-jun-1772', 'west england'),
+('billy bob', '12-july-1987', 'seattle  washington'),
+('mike watts', '4-mar-1967', 'new york'),
+('lip galmore', '5-dec-1994', 'west texas');
